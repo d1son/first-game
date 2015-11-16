46f04f4de74d2df943bd224544e8131a29c6c011
Dong Son <d1son@Dongs-MBP.home>
2015-11-16 15:58:50 -0500

    added the counter function, timer function, added additional css styling

--- js/click.js
+++ js/click.js
@@ -1,19 +1,57 @@
-var imageElements = document.getElementsByTagName('img');
+var decreaseSeconds;
+var secondsLeft = 20;
+var count = 0;
+var img = document.getElementsByClassName("clickImage");
 
-for (var i = 0; i < imageElements.length; i++) {
-	imageElements[i].addEventListener("click", function() {
-		function clickdone() {
-		  var hero = document.getElementById("hero");
-		  var counter = document.getElementById("counter");
-		  var count = counter.innerHTML;
-		  count++;
-		  counter.innerHTML = count;
-		}
-	});
+// Start the timer function
+
+function startTimer() {
+	decreaseSeconds = setInterval(countDown, 1000);
+	for (var i = 0; i < img.length; i++) {
+	img[i].setAttribute("data-click", "true");
+	}
 }
 
+document.getElementById("startButton").addEventListener("click", startTimer)
 
+//Count down function
 
+function countDown(){
+  if(secondsLeft > 0){
+    document.getElementById("seconds").innerHTML = secondsLeft-=1;
+  } else {
+      document.getElementById("timeLeft").innerHTML = "Your time is up!";
+      document.getElementById("timeLeft").className = "change";
+      alert("You clicked " + count + " Pictures");
+      alert("Game Over! Play again");
+      for (var i = 0; i < img.length; i++) {
+       img[i].setAttribute("data-click", "false");
+      };
+      clearInterval(decreaseSeconds);
+  };
+}; 
+
+//Count number of clicks
+
+function countClicks(){      
+    if (this.getAttribute('data-click') == 'true'){
+      count++;
+      this.setAttribute('data-click', 'false');
+      this.className = "clicked";
+      document.getElementById("clickCount").innerHTML = count;
+    };
+    
+    	if (count === 20) {
+      alert("You've clicked all " + count + " pictures with " + secondsLeft + " seconds left on the clock!")
+      alert("Game Over! Play again")
+      clearInterval(decreaseSeconds);
+      
+    };
+  };
+
+ for (i = 0; i < 20; i++){
+    document.getElementsByClassName("clickImage")[i].addEventListener("click", countClicks);
+  };
 
 
 //document.getElementById("startButton").addeventlistener("click", startTimer);
