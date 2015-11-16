c299f323cb93a7aee2c8afd18a285225018672d4
Dong Son <d1son@Dongs-MBP.home>
2015-11-16 00:06:30 -0500

    added for loop to activate funtion when img is clicked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.getElementById("startButton")
+var imageElements = document.getElementsByTagName('img');
+
+
+
+for (var i = 0; i < imageElements.length; i++) {
+	imageElements[i].addEventListener("click", function() {
+		alert("you clicked");
+	});
+}
+
+
+//document.getElementById("startButton").addeventlistener("click", startTimer);
+//document.getElementsByClassName("thumbnail").addeventlistener("click", startCounter);
